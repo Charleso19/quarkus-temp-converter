3544048d86d551dcb4b254c2850216c702aa4b49
Owen Charles <Charleso19@users.noreply.github.com>
2021-12-24 22:05:21 +0000

    Implemented Mutiny and added tests.

--- src/main/java/org/acme/TemperatureConverterResource.java
+++ src/main/java/org/acme/TemperatureConverterResource.java
@@ -1,10 +1,14 @@
 package org.acme;
 
+import io.smallrye.mutiny.Uni;
 import org.acme.service.TemperatureConverterService;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/convert")
@@ -19,23 +23,28 @@ public class TemperatureConverterResource {
     @Path("/celsius/{degrees}")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String celsiusToFahrenheit(
+    public Uni<String> celsiusToFahrenheit(
             @PathParam(value = "degrees") final String degrees) {
 
         LOGGER.infof("Degrees in Celsius: %s\u00B0C", degrees);
 
-        return String.valueOf(temperatureConverterService.celsiusToFahrenheit(degrees));
+        return Uni.createFrom().item(degrees)
+                .onItem()
+                .transform(item ->
+                        temperatureConverterService.celsiusToFahrenheit(item));
     }
 
     @Path("/fahrenheit/{degrees}")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String fahrenheitToCelsius(
+    public Uni<String> fahrenheitToCelsius(
             @PathParam(value = "degrees") final String degrees) {
 
         LOGGER.infof("Degrees in Fahrenheit: %s\u00B0F", degrees);
 
-        return String.valueOf(temperatureConverterService.fahrenheitToCelsius(degrees));
-
+        return Uni.createFrom().item(degrees)
+                .onItem()
+                .transform(item ->
+                        temperatureConverterService.fahrenheitToCelsius(item));
     }
 }
